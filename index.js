5e6142f67f46415bbe71ab2f7712b934dae703a9
Ryan Bruner <ryan.bruner87@gmail.com>
2023-05-15 16:25:50 -0400

    update arrays

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const arrayStart = [];
+const arrayStart = ['Good Morning! Let\'s get the day started with ', 'Happy Afternoon! Let\'s catch-up and do  ', 'Evening has arrived! Don\'t delay; get working with ' ];
 const arrayMiddle = [];
 const arrayEnd = [];
+
+const getSentenceStart = () => {
+
+};
+
+const getSentenceMiddle = () => {
+
+};
+
+const getSentenceEnd = () => {
+
+};
 
